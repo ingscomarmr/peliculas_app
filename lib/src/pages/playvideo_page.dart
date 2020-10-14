49849777474eda2354a1b7b29ab05a0cfb283d6c
ingscomarmr <ingscomarmr@gmail.com>
2020-10-13 19:56:05 -0500

    ajustes para video live

--- lib/src/pages/playvideo_page.dart
+++ lib/src/pages/playvideo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 //import 'package:video_player/video_player.dart';
 
 class PlayVideoPage extends StatefulWidget {
@@ -9,18 +10,71 @@ class PlayVideoPage extends StatefulWidget {
 }
 
 class _PlayVideoPageState extends State<PlayVideoPage> {
-  //VideoPlayerController _controller;
-  //Future<void> _initializeVideoPlayerFuture;
+  String videUrl =
+      'https://cdn3.wowza.com/1/OVN4RDQwZHc3RkE1/L2IvWGlT/hls/live/playlist.m3u8';
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    print("Inicializa control VideoPlayerController");
+    _controller = VideoPlayerController.network(videUrl);
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ver trailer Oficial'),
-      ),
-      body: Container(
-        child: Text('Playing....'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Ver trailer Oficial'),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: FutureBuilder(
+                future: _initializeVideoPlayerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    // If the VideoPlayerController has finished initialization, use
+                    // the data it provides to limit the aspect ratio of the VideoPlayer.
+                    return AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      // Use the VideoPlayer widget to display the video.
+                      child: VideoPlayer(_controller),
+                    );
+                  } else {
+                    // If the VideoPlayerController is still initializing, show a
+                    // loading spinner.
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              // If the video is playing, pause it.
+              if (_controller.value.isPlaying) {
+                _controller.pause();
+              } else {
+                // If the video is paused, play it.
+                _controller.play();
+              }
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ));
   }
 }
