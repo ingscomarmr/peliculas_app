34111ab3e7db85c85b437ac8324d9a08bceedc46
Omar Munguia <ingscomarmr@gmail.com>
2020-10-27 23:35:18 -0600

    clase:113 CustomerScrollView y SliverAppBar

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -8,11 +8,31 @@ class PeliculaDetalle extends StatelessWidget {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Detalles Pelicula'),
+      body: CustomScrollView(
+        slivers: <Widget>[_crearAppBar(pelicula)],
       ),
-      body: Center(
-        child: Text('Detalles pelicula: ${pelicula.title}'),
+    );
+  }
+
+  Widget _crearAppBar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200.0,
+      floating: true,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+        ),
+        background: FadeInImage(
+          image: NetworkImage(pelicula.getUrlImgBackground()),
+          placeholder: AssetImage('assets/img/loading.gif'),
+          /*fadeInDuration: Duration(microseconds: 150),*/
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
